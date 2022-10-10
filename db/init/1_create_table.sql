52368c5b907dc5913d5647712c645b3f53a65ea0
yamasaki <yamasakipann0218@gmail.com>
2022-10-10 20:38:01 +0900

    docker 構築C

---
+++ db/init/1_create_table.sql
@@ -0,0 +1,17 @@
+-- DB切り替え
+\c "hoge"
+
+
+CREATE TABLE "articles" (
+  "id"                 SERIAL NOT NULL PRIMARY KEY,
+  "title"              VARCHAR(255) NOT NULL,
+  "likes"              INT NOT NULL,
+  "url"                VARCHAR(255) NOT NULL,
+  "author"             VARCHAR(255) NOT NULL,
+  "kind"               VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "users" (
+  "id"                VARCHAR(255) NOT NULL PRIMARY KEY,
+  "name"              VARCHAR(255) NOT NULL
+);
