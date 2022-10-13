7f6395397a1f356da6d57107f4e5019dd65df95c
yamasaki <yamasakipann0218@gmail.com>
2022-10-13 20:15:44 +0900

    infra repository 雛形作成

--- infra/repository/user.go
+++ infra/repository/user.go
@@ -1,1 +1,34 @@
 package repository
+
+import (
+	"User-API/domain/entity"
+	"User-API/domain/repository"
+	"context"
+	"database/sql"
+)
+
+type userRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) repository.UserRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+func (ur userRepository) CreateUser(context.Context, *entity.User) (*entity.User, error) {
+	return nil, nil
+}
+
+func (ur userRepository) UpdateUser(context.Context, *entity.User) (*entity.User, error) {
+	return nil, nil
+}
+
+func (ur userRepository) GetUser(ctx context.Context, id string) (*entity.User, error) {
+	return nil, nil
+}
+
+func (ur userRepository) DeleteUser(ctx context.Context, id string) (*entity.User, error) {
+	return nil, nil
+}
