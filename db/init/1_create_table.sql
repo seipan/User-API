33182a8d464cd6950d787e2dd12127b393382968
yamasaki <yamasakipann0218@gmail.com>
2022-10-17 21:18:10 +0900

    sql password追加

--- db/init/1_create_table.sql
+++ db/init/1_create_table.sql
@@ -4,5 +4,6 @@
 CREATE TABLE "users" (
   "id"                VARCHAR(255) NOT NULL PRIMARY KEY,
   "name"              VARCHAR(255) NOT NULL,
-  "mail"              VARCHAR(255) NOT NULL
+  "mail"              VARCHAR(255) NOT NULL,
+  "password"              VARCHAR(255) NOT NULL
 );
