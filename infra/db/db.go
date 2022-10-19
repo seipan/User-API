fae89af10d17c684396c2183144a9d5c4d2849b0
yamasaki <yamasakipann0218@gmail.com>
2022-10-19 17:43:35 +0900

    db 接続

--- infra/db/db.go
+++ infra/db/db.go
@@ -1,1 +1,17 @@
 package db
+
+import (
+	"database/sql"
+	"log"
+)
+
+func NewDriver() *sql.DB {
+	db, err := sql.Open("postgres", "host=127.0.0.1 port=5555 user=root password=password dbname=testdb sslmode=disable")
+	if err != nil {
+		log.Println("db connect failed")
+		panic(err)
+	}
+	log.Println("db connect success")
+
+	return db
+}
