0c152c0d82c4f21ffc4e9dc3adc504e03adcc948
yamasaki <yamasakipann0218@gmail.com>
2022-10-19 18:22:11 +0900

    handler ひな形作成

--- web/handler/user.go
+++ web/handler/user.go
@@ -1,1 +1,35 @@
 package handler
+
+import (
+	"User-API/usecase"
+	"net/http"
+)
+
+type UserHandler interface {
+	CreateUser(http.ResponseWriter, *http.Request)
+	UpdateUser(http.ResponseWriter, *http.Request)
+	GetUser(http.ResponseWriter, *http.Request)
+	DeleteUser(http.ResponseWriter, *http.Request)
+}
+
+type userHandler struct {
+	userUsecase usecase.UserUsecase
+}
+
+func NewUserhandler(uu usecase.UserUsecase) UserHandler {
+	return userHandler{
+		userUsecase: uu,
+	}
+}
+
+func (uh userHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+}
+
+func (uh userHandler) UpdateUser(w http.ResponseWriter, r *http.Request) {
+}
+
+func (uh userHandler) GetUser(w http.ResponseWriter, r *http.Request) {
+}
+
+func (uh userHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+}
