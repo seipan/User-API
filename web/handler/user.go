3d5f455e369340c5098416e8673e3216af4be2bf
yamasaki <yamasakipann0218@gmail.com>
2022-10-20 19:50:45 +0900

    create user

--- web/handler/user.go
+++ web/handler/user.go
@@ -1,8 +1,12 @@
 package handler
 
 import (
+	"User-API/domain/entity"
+	handler_error "User-API/error/handler"
 	"User-API/usecase"
+	"User-API/utils"
 	"net/http"
+	"strconv"
 )
 
 type UserHandler interface {
@@ -23,6 +27,48 @@ func NewUserhandler(uu usecase.UserUsecase) UserHandler {
 }
 
 func (uh userHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		utils.CreateErrorResponse(w, r, "method not allowed", handler_error.MethodNotAllowd)
+		return
+	}
+
+	newName := r.FormValue("name")
+	if newName == "" {
+		utils.CreateErrorResponse(w, r, "name empty", nil)
+		return
+	}
+
+	newId := r.FormValue("id")
+	if newId == "" {
+		utils.CreateErrorResponse(w, r, "id empty", nil)
+		return
+	}
+
+	newMail := r.FormValue("mail")
+	if newMail == "" {
+		utils.CreateErrorResponse(w, r, "id empty", nil)
+		return
+	}
+
+	newintId, err := strconv.ParseInt(newId, 10, 64)
+
+	if err != nil {
+		utils.CreateErrorResponse(w, r, "id not number", nil)
+		return
+	}
+
+	newUser := &entity.User{}
+	newUser.Id = newintId
+	newUser.Name = newName
+	newUser.Mail = newMail
+
+	user, err := uh.userUsecase.CreateUser(r.Context(), newUser)
+
+	if err != nil {
+		utils.CreateErrorResponse(w, r, "faild to createuser", err)
+		return
+	}
 }
 
 func (uh userHandler) UpdateUser(w http.ResponseWriter, r *http.Request) {
