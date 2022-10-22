836286702443dfdd42b2d48ead92fae6e67c515a
yamasaki <yamasakipann0218@gmail.com>
2022-10-22 20:41:36 +0900

    response 作成

--- web/response/user.go
+++ web/response/user.go
@@ -1,1 +1,17 @@
 package response
+
+import "User-API/domain/entity"
+
+func NewUserResponse(user *entity.User) UserResponse {
+	return UserResponse{
+		Name: user.Name,
+		Id:   user.Id,
+		Mail: user.Mail,
+	}
+}
+
+type UserResponse struct {
+	Name string `json:"name"`
+	Id   int64  `json:"id"`
+	Mail string `json:"mail"`
+}
