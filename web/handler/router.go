418feba35a026e7d35be997cfeca1105cf81c5b8
yamasaki <yamasakipann0218@gmail.com>
2022-10-24 22:55:45 +0900

    routing 追加

--- web/handler/router.go
+++ web/handler/router.go
@@ -1,1 +1,20 @@
 package handler
+
+import (
+	"User-API/web/middleware"
+	"net/http"
+)
+
+func InitRouter(userHandler UserHandler) {
+	userCreate := http.HandlerFunc(userHandler.CreateUser)
+	http.Handle("/user/create", middleware.Layres(userCreate))
+
+	userUpdate := http.HandlerFunc(userHandler.UpdateUser)
+	http.Handle("/user/update", middleware.Layres(userUpdate))
+
+	userGet := http.HandlerFunc(userHandler.GetUser)
+	http.Handle("/user/get", middleware.Layres(userGet))
+
+	userDelete := http.HandlerFunc(userHandler.DeleteUser)
+	http.Handle("/user/delete", middleware.Layres(userDelete))
+}
