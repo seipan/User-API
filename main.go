60aa34d3664edcef980eaf947b7cda24d3e689aa
yamasaki <yamasakipann0218@gmail.com>
2022-10-24 23:11:25 +0900

    main 関数完成

--- main.go
+++ main.go
@@ -1,2 +1,28 @@
 package main
 
+import (
+	"User-API/infra/db"
+	"User-API/infra/repository"
+	"User-API/usecase"
+	"User-API/web/handler"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db := db.NewDriver()
+	defer db.Close()
+
+	userRepository := repository.NewUserRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	userHandler := handler.NewUserhandler(userUsecase)
+
+	handler.InitRouter(userHandler)
+	srv := &http.Server{
+		Addr: ":8000",
+	}
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalln("Server closed with error :", err)
+	}
+}
