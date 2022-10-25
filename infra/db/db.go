bbe8de6746bf1f2e9030f25accd79161a7894b81
yamasaki <yamasakipann0218@gmail.com>
2022-10-25 20:52:25 +0900

    postgres 周りの設定完了

--- infra/db/db.go
+++ infra/db/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func NewDriver() *sql.DB {
-	db, err := sql.Open("postgres", "host=127.0.0.1 port=5555 user=root password=password dbname=testdb sslmode=disable")
+	db, err := sql.Open("postgres", "host=127.0.0.1 port=5555 user=hoge password=hoge dbname=testdb sslmode=disable")
 	if err != nil {
 		log.Println("db connect failed")
 		panic(err)
